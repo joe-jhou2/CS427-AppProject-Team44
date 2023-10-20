aabc6252432344c01479541f52d60aedcddf44bc
Erichuangyb <68992418+Erichuangyb@users.noreply.github.com>
2023-10-19 21:49:20 -0500

    Add files via upload
    Here is a basic activity class for our settings page. I created two buttons. One for logging off and one for changing themes.

---
+++ app/src/main/java/edu/uiuc/cs427app/SettingsActivity.java
@@ -0,0 +1,43 @@
+package com.example.cs427milestone3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Button changeThemeButton, signOutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        changeThemeButton = findViewById(R.id.changeThemeButton);
+        signOutButton = findViewById(R.id.signOutButton);
+
+        // Created a button for changing theme
+        changeThemeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic to change theme
+            }
+        });
+
+        // Jump to the Authentication Page(Create AccountActivity in our case)
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If you're using shared preferences or any other method for session management, clear the session details here.
+
+                // Redirect to Authentication Page(Create AccountActivity in our case)
+                Intent intent = new Intent(SettingsActivity.this, CreateAccountActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // This makes sure the user can't navigate back to previous activities using the back button
+                startActivity(intent);
+            }
+        });
+    }
+}
