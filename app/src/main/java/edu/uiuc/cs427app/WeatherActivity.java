aae651d13669bdc5ed96e7cdd759812c6d66f556
ericillinois <zhaol4@illinois.edu>
2023-10-31 00:32:36 -0400

    create WeatherActivity, need to implement it later

--- app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
@@ -1,18 +1,6 @@
 package edu.uiuc.cs427app;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-
-import retrofit2.Response;
 
 
 public class WeatherActivity extends AppCompatActivity {
