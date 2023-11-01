6f83264d4e7da1e1ff0b56c3b50439005723939d
ericillinois <zhaol4@illinois.edu>
2023-10-31 23:29:26 -0400

    get the weather for one city

--- app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
@@ -2,55 +2,99 @@ package edu.uiuc.cs427app;
 
 import android.accounts.Account;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 //import java.net.http.HttpResponse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "http://api.weatherapi.com/v1/current.json";
+    private static final String BASE_URL = "https://api.weatherapi.com/v1/current.json";
     private static final String API_KEY = "1c9ca48edcd6459aaa514033233110";
     private static String cityname;
 
     private Account account;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         account = getIntent().getParcelableExtra("account");
-
-        // Apply the theme based on the user's preference
         ThemeUtils.applyTheme(account, this);
         setContentView(R.layout.activity_weather);
 
-        // Process the Intent payload that has opened this Activity and show the information accordingly
         String cityName = getIntent().getStringExtra("city");
         String welcome = "Weather in " + cityName;
-
-        // TODO: Get the actual date and time
         String date_and_time = "9:00 AM, October 31st, 2023";
 
-        // Initializing the GUI elements
         TextView welcomeMessage = findViewById(R.id.welcomeText);
         TextView date = findViewById(R.id.date_and_time);
         welcomeMessage.setText(welcome);
         date.setText(date_and_time);
 
-        // Get the weather information from a Service that connects to a weather server and show the results
         String solo_name = cityName.split(",", 2)[0];
-        System.out.println(solo_name);
-        String full_url = BASE_URL + "?key=" + API_KEY + "?q=" + solo_name;
-        System.out.println(full_url);
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create(BASE_URL + "?key=" + API_KEY + "?q=" + SOLO_NAME))
-//                .method("GET", HttpRequest.BodyPublishers.noBody())
-//                .build();
-//
-//        HttpResponse<String> response = null;
-//        response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        TextView weatherInfo = findViewById(R.id.temperature);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String apiUrl = BASE_URL + "?key=" + API_KEY + "&q=" + solo_name;
+
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(apiUrl)
+                        .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        String responseBody = response.body().string();
+                        JSONObject json = new JSONObject(responseBody);
+
+                        // Extract weather information from the JSON response
+                        String city = json.getJSONObject("location").getString("name");
+                        String temperature = json.getJSONObject("current").getString("temp_c");
+                        String weatherDescription = json.getJSONObject("current").getJSONObject("condition").getString("text");
+
+                        // Now, use Handler to post the UI update back on the main thread
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                weatherInfo.setText("Weather in " + city + ": " + weatherDescription + ", " + temperature + "°C");
+                            }
+                        });
+                    } else {
+                        // Handle API request error
+                        // Optionally, use Handler to show an error message on the main thread
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("WeatherActivity", "Network error: " + e.getMessage());
+                    // Handle network or other errors
+                    // Optionally, use Handler to show an error message on the main thread
+                }
+            }
+        }).start();
     }
+
 }
