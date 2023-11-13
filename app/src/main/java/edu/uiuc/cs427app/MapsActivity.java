f05f6edb556497b74cabe7ec8ee01fc2cd0299a2
jeff-hank <jdhank2@illinois.edu>
2023-11-12 19:51:24 -0600

    comments

--- app/src/main/java/edu/uiuc/cs427app/MapsActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MapsActivity.java
@@ -31,9 +31,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //apply use theme to this activity
         Account account = getIntent().getParcelableExtra("account");
         ThemeUtils.applyTheme(account, this);
 
+        //bind the map to the view
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -48,12 +50,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         latitude = intent.getDoubleExtra("lat",-34);
         longitude = intent.getDoubleExtra("lon",151);
 
+        //display city information
         String msg = cityname + "\n" +
                 "Latitude: " +String.format("%.5f", latitude)
                 + "\n" +
                 "Longitude: "+String.format("%.5f", longitude);
-
-
         TextView cityInfo = findViewById(R.id.cityNameLatLon);
         cityInfo.setText(msg);
 
@@ -61,30 +62,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used. We add a marker with the cityname
-     * and move the map view camera to the city's latitude and longitude coords. Map preferences are
+     * This callback is triggered when the map is ready to be used. We move the map
+     * view camera to the city's latitude and longitude coords. Map preferences are
      * saved in activity_maps.xml
      * @param googleMap  the map to be drawn
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //set the map lat/lon, add a market with cityname, and move the camera to the city
 
+        //set the map lat/lon, and move the camera to the city
         LatLng citylocation = new LatLng(latitude, longitude);
-
-        String msg = cityname + "\n" +
-                     "Latitude: " +String.format("%.5f", latitude)
-                     + "\n" +
-                     "Longitude: "+String.format("%.5f", longitude);
-
-        IconGenerator iconFactory = new IconGenerator(this);
-        MarkerOptions markerOptions = new MarkerOptions().
-                icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon(msg))).
-                position(citylocation).
-                anchor(iconFactory.getAnchorU(), iconFactory.getAnchorV());
-
-        //mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(citylocation));
     }
 
